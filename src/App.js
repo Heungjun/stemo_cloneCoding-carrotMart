1d319266ec4761cab974ae3f7c7657ca686f257f
Heungjun <gmdwns92@gmail.com>
2022-04-14 19:41:22 +0900

    홈 화면 및 홈 화면 테스트 용 데이터

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./common.css";
+import MobileAppBar from "./component/MobileAppBar";
+import MobileHomeList from "./component/MobileHomeList";
+import "./component/MobileNavigation";
+import MobileNavigation from "./component/MobileNavigation";
+import testDatas from "./data/HomeListData";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MobileAppBar />
+      <section>
+        <MobileHomeList datas={testDatas} />
+      </section>
+      <MobileNavigation />
     </div>
   );
 }
---
+++ src/data/HomeListData.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    imagePath: "",
+    title: "땡땡이 블라우스",
+    address: "연제구 거제동",
+    price: "15000",
+  },
+  {
+    imagePath: "",
+    title: "크리넥스 어린이 입체 마스크 19개",
+    address: "사하구 괴정제4동",
+    price: "7000",
+  },
+  {
+    imagePath: "",
+    title: "사시거나 교환가능해요",
+    address: "부산진구 개금동",
+    price: "1111",
+  },
+  {
+    imagePath: "",
+    title: "크리넥스 어린이 입체 마스크 19개",
+    address: "사하구 괴정제4동",
+    price: "7000",
+  },
+  {
+    imagePath: "",
+    title: "문구류 몽땅(거의다새거)",
+    address: "사하구 하단제1동",
+    price: "2000",
+  },
+  {
+    imagePath: "",
+    title: "옛날돈팝니다(완미)",
+    address: "동구 수정제2동",
+    price: "20000",
+  },
+  {
+    imagePath: "",
+    title: "바른 필라테스 양도(대신동)",
+    address: "서구 서대신동3가",
+    price: "460000",
+  },
+  {
+    imagePath: "",
+    title: "띠부실 판매합니다",
+    address: "덕포동",
+    price: "100",
+  },
+];

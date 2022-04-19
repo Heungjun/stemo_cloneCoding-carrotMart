c876b16c2c8ef8975feb61cd3394e1cd10dbcae4
Yun Heung Jun <hj.yun@impix.co.kr>
2022-04-19 14:15:55 +0900

    Add MobileHomeListItem, sale status

--- src/component/MobileHomeListItem.js
+++ src/component/MobileHomeListItem.js
@@ -1,46 +1,77 @@
-import React from "react";
+import React from 'react';
 
 const MobileHomeListItem = ({
-  /**
-   *  Type: String
-   *  required
-   *  Description: Image Path
-   */
-  imagePath,
+	data: {
+		/**
+		 *  Type: String
+		 *  required
+		 *  Description: Image Path
+		 */
+		imagePath,
 
-  /**
-   *  Type: String
-   *  required
-   *  Description: title
-   */
-  title,
+		/**
+		 *  Type: String
+		 *  required
+		 *  Description: title
+		 */
+		title,
 
-  /**
-   *  Type: String
-   *  required
-   *  Description: address
-   */
-  address,
+		/**
+		 *  Type: String
+		 *  required
+		 *  Description: address
+		 */
+		address,
 
-  /**
-   *  Type: Number
-   *  required
-   *  Description: price
-   */
-  price,
+		/**
+		 *  Type: Number
+		 *  required
+		 *  Description: price
+		 */
+		price,
+
+		/**
+		 *  Type: Number
+		 *  Default: 0
+		 *  Description: ["판매중", "예약중", "판매완료"]
+		 */
+		status = 0,
+	},
 }) => {
-  return (
-    <li className="mobile-home-list-item">
-      <img className="mobile-home-list-item__image" src={imagePath} />
-      <div className="mobile-home-list-item__description">
-        <div className="mobile-home-list-item__title">{title}</div>
-        <div className="mobile-home-list-item__address">{address}</div>
-        <div className="mobile-home-list-item__price">
-          {price ? price.toLocaleString("ko-KR") + "원" : "나눔"}
-        </div>
-      </div>
-    </li>
-  );
+	const statusView = (status) => {
+		switch (status) {
+			case 1:
+				return (
+					<div className="mobile-home-list-item__status mobile-home-list-item__status--stay">
+						예약중
+					</div>
+				);
+			case 2:
+				return (
+					<div className="mobile-home-list-item__status mobile-home-list-item__status--done">
+						판매완료
+					</div>
+				);
+			default:
+				return;
+		}
+	};
+
+	return (
+		<li className="mobile-home-list-item">
+			<img className="mobile-home-list-item__image" src={imagePath} />
+			<div className="mobile-home-list-item__description">
+				<div className="mobile-home-list-item__title">{title}</div>
+				<div className="mobile-home-list-item__address">{address}</div>
+				<div className="mobile-home-list-item__l3">
+					{statusView(status)}
+					<div className="mobile-home-list-item__price">
+						{price ? price.toLocaleString('ko-KR') + '원' : '나눔'}
+					</div>
+				</div>
+			</div>
+		</li>
+	);
 };
 
 export default MobileHomeListItem;

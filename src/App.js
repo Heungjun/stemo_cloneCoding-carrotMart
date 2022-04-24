dd1c1a6dc2a97c58f8ea520e8795a26fba1117e4
heungjunyun <gmdwns92@gmail.com>
2022-04-24 17:43:19 +0900

    New Chat

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { useState } from "react";
 import "./common.css";
 import "./component/MobileNavigation";
 import MobileNavigation from "./component/MobileNavigation";
+import ChatPage from "./pages/ChatPage";
 import HomePage from "./pages/HomePage";
 
 function App() {
   const tabView = (index) => {
     switch (index) {
+      case 3: return <ChatPage />;
       case 0:
       default:
         return <HomePage />;
